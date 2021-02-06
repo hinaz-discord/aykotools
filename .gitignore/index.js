2905d8d9c0f7cf06dee66f3ed1c4b38ba303519b
hinaz-discord <77556989+hinaz-discord@users.noreply.github.com>
2021-02-06 12:35:45 +0100

    Create index.js

---
+++ .gitignore/index.js
@@ -0,0 +1,23 @@
+{
+
+    "login": { 
+     
+        "token": "ODAyNTYzNTIxNzY5MzczNjk2.YAxDhg.aGyP9TX9ckJfsf5Tqr612tLBboc"
+
+    },
+    "bot": { 
+        "owner": "579715947720605755",
+        "dev": [
+            "<@579715947720605755>"
+          
+        ],
+
+        
+        "clientid": "768164233187557479",
+         "version": "ayko bot (v0.3)",
+         "support": "❌",
+         "image": "https://cdn.discordapp.com/avatars/553990268878061598/a_337c293f1a938faf0c4a502b7ef809a9.gif",
+         "fhouther" : "ayko Bot • version: ayko bot (v0.3) • Fin du bot"
+
+    }
+}
